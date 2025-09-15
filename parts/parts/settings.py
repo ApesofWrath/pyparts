86f6f9791f119a9d177be06a46bcbb7c15628e38
Rahil Mehta <rahil@Mac.localdomain>
2025-09-15 09:51:11 -0700

    fix: Fix static files serving in Docker deployment
    - Update nginx configurations to use correct static file paths (/app/static/)
    - Fix Django static files settings to include both static directories
    - Add STATICFILES_FINDERS configuration for proper static file discovery
    - Remove volume mount that was overriding static files in production
    - Ensure static files are properly collected and served by nginx
    - Static files should now display correctly in deployed containers

--- parts/parts/settings.py
+++ parts/parts/settings.py
@@ -186,9 +186,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = "static"
+STATIC_ROOT = "/app/static"
 STATIC_URL = "/static/"
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "parts_site", "static"),
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
